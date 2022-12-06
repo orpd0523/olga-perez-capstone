a019a7508f6f282027e7f5966e1f031a6000b8a9
orpd0523 <orpd0523@gmail.com>
2022-12-06 02:46:15 -0600

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <span>
+        <div>Logo Here, Maybe?
+        </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </span>
+  );
+}
+
+export default Header;
