8f569ac90e3bc59b1aec84ae0a3722c7da9ea635
orpd0523 <orpd0523@gmail.com>
2022-12-11 23:06:36 -0600

    working on animations for avatar

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,9 +1,9 @@
 import "./Avatar.scss";
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
 import spriteSheet from "../../assets/tamagotchi-sprite-sheet.png";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-import { useState } from "react";
+import { useRef } from "react";
 
 function Avatar() {
   return (
@@ -14,28 +14,61 @@ function Avatar() {
     </div>
   );
 }
-
-const mametchiFrames = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-];
+const avatarObj = {
+  id: 1,
+  frames: [
+    {
+      x: 0,
+      y: 22,
+    },
+    {
+      x: 1,
+      y: 22,
+    },
+    {
+      x: 4,
+      y: 22,
+    },
+    {
+      x: 8,
+      y: 22,
+    },
+    {
+      x: 11,
+      y: 22,
+    },
+  ],
+  idle: {
+    frames: [5],
+    repeat: null,
+  },
+  dancing: {
+    frames: [1, 2],
+    repeat: 3,
+  },
+  upset: {
+    frames: [4],
+    repeat: null,
+  },
+};
 
 const FRAMES = 32;
 const MAX_POSITION = 31;
 const MIN_POSITION = 0;
 
 function Sprite() {
-  const [xPos, setXPos] = useState(0);
+  const xCoord = useRef(11);
+  const yCoord = useRef(22);
   const spriteMap = useLoader(TextureLoader, spriteSheet);
   spriteMap.repeat.set(1 / FRAMES, 1 / FRAMES); //rows, columns
-  spriteMap.offset.x = xPos / FRAMES; //left to right
-  spriteMap.offset.y = 22 / FRAMES; //up and down
-  setInterval(() => {
-    setXPos((current) => {
-      let newX = current + 1;
-      newX = newX > 4 ? 0 : newX;
-      return newX;
-    });
-  }, 2000);
+  spriteMap.offset.x = xCoord.current / FRAMES; //left to right
+  spriteMap.offset.y = yCoord.current / FRAMES; //up and down
+  // setInterval(() => {
+  //   avatarObj.frames.forEach(({x, y}) => {
+  //     spriteMap.offset.x = x / FRAMES
+  //     spriteMap.offset.y = y / FRAMES
+  //   });
+  // }, 2000); 
   return (
     <mesh>
       <planeGeometry args={[4, 4]} />
