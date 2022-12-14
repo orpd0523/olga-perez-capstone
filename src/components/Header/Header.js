a895cd43036ee4aad7c9a74ae27671e79858e6d6
orpd0523 <orpd0523@gmail.com>
2022-12-14 01:06:52 -0600

    completed responsiveness and styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,15 @@ function Header() {
   return (
     <header className="header">
       <nav className="header__nav">
-        <h1 className="header__logo">To Do Today: </h1><h2 className="header__logo-2">Stay Alive</h2>
+        <span className="header__logo-container">
+          <h1 className="header__logo">To Do Today: </h1>
+          <h2 className="header__logo-2">Stay Alive</h2>
+        </span>
         <ul className="header__menu">
           <li className="header__item">Home</li>
-          <li className="header__item" onClick={toggle}>Avatar</li>
+          <li className="header__item" onClick={toggle}>
+            Avatar
+          </li>
         </ul>
       </nav>
     </header>
