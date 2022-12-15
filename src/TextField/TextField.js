d3e74cbfe583bc351d4ae86b523515ddcc99c7d1
orpd0523 <112235150+orpd0523@users.noreply.github.com>
2022-12-14 22:02:34 -0600

    final commit 4.0

---
+++ src/TextField/TextField.js
@@ -0,0 +1,36 @@
+import "./TextField.scss";
+
+function TextField(props) {
+  const {
+    id,
+    type = "text",
+    label,
+    placeholder = props.label,
+    name,
+    component = "input",
+    ...otherProps
+  } = props;
+
+  const Input = component;
+  let className = "text-field__input";
+  if (component !== "input") {
+    className = `text-field__input text-field__input--${component}`;
+  }
+  return (
+    <div className="text-field">
+      <label htmlFor={id} className="text-field__label">
+        {label}
+      </label>
+      <Input
+        {...otherProps}
+        name={name}
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        className={className}
+      />
+    </div>
+  );
+}
+
+export default TextField;
